b0b0e89a0f20ed3356afcc554408d49a88548f9c
ShamithaSuriakumar <85447602+ShamithaSuriakumar@users.noreply.github.com>
2021-06-12 20:03:34 +0530

    Positive Numbers in a Range.py
    This code will print Positive Numbers in a Range

---
+++ Positive Numbers in a Range.py
@@ -0,0 +1,11 @@
+l1 = [12,-7,5,64,-14]
+l2 = [12,14,-95,3]
+l = [ ]
+for i in range(len(l1)):
+	if l1[i] > 0:
+		print(l1[i],end=",")
+print()
+for i in range(len(l2)):
+	if l2[i] > 0:
+	  l.append(l2[i])
+print(l)
